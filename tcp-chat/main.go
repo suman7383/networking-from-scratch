9448784c1f7410728d8861c361ca6c130239e559
suman <sumanmukherjee2001@gmail.com>
2026-01-07 21:37:10 +0530

    Add tcp-chat

---
+++ tcp-chat/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+type ChatServer struct {
+	clients []net.Conn
+	ln      net.Listener
+}
+
+// Creates a new Chat server
+func NewChatServer(port string) (*ChatServer, error) {
+	ln, err := net.Listen("tcp", port)
+
+	if err != nil {
+		fmt.Println("Error starting the server")
+		return nil, err
+	}
+
+	return &ChatServer{
+		clients: make([]net.Conn, 0),
+		ln:      ln,
+	}, nil
+}
+
+func (c *ChatServer) Start() error {
+	fmt.Printf("[SERVER] started accepting connections on: %s\n", c.ln.Addr())
+
+	for {
+		conn, err := c.ln.Accept()
+
+		if err != nil {
+			fmt.Printf("[ERROR] could not accept client connection from %s\n", conn.RemoteAddr())
+			continue
+		}
+
+		fmt.Printf("[SERVER] new client connected: %s\n", conn.RemoteAddr())
+
+		// Push this client to clients slice
+		c.clients = append(c.clients, conn)
+
+		// Handle the connection
+		go c.handleConnection(conn)
+	}
+}
+
+// Handle connection life cycle
+func (c *ChatServer) handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	// Read from connection
+	for {
+		bytes, err := reader.ReadBytes(byte('\n'))
+
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("[ERROR] reading from connection %s, err: %s\n", conn.RemoteAddr(), err)
+			} else {
+				fmt.Printf("[ERROR] client connection closed %s\n", conn.RemoteAddr())
+			}
+
+			return
+		}
+
+		// Broadcast the message to all
+		msg := fmt.Sprintf("[CLIENT] %s", bytes)
+		c.broadcastExceptSelf(conn, []byte(msg))
+	}
+}
+
+func (c *ChatServer) broadcastExceptSelf(conn net.Conn, msg []byte) {
+	// iterate over clients slice
+	for _, client := range c.clients {
+		// Skip the sender
+		if client == conn {
+			continue
+		}
+
+		_, err := client.Write(msg)
+
+		if err != nil {
+			fmt.Printf("[ERROR] writing to client, %s\n", err)
+			continue
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Expected 2 arguments got %d\n", len(os.Args))
+		os.Exit(1)
+	}
+
+	// Format the port properly(:8080)
+	port := fmt.Sprintf(":%s", os.Args[1])
+
+	server, err := NewChatServer(port)
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// Start accepting connections
+	server.Start()
+}
