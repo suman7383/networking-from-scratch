606c38cb2d1f710f209d1fe0f78603fd32c940d0
suman <sumanmukherjee2001@gmail.com>
2026-01-10 23:12:57 +0530

    Add NewHttpServer function

--- http-server/http-server.go
+++ http-server/http-server.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 // TODO
 type HttpServer struct {
+	ln net.Listener
+}
+
+func NewHttpServer(port string) (*HttpServer, error) {
+	ln, err := net.Listen("tcp", port)
+
+	if err != nil {
+		fmt.Println("Error creating http server, err:", err)
+		return nil, err
+	}
+
+	return &HttpServer{
+		ln: ln,
+	}, nil
 }
 
 // TODO
