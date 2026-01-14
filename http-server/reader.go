6dd8c046444bab05926076919cb95272189a1e77
suman <sumanmukherjee2001@gmail.com>
2026-01-14 21:01:17 +0530

    Add ReadLine, ReadN

--- http-server/reader.go
+++ http-server/reader.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"net"
+	"strings"
 )
 
 // Reader is responsible for reading from the connection
@@ -17,14 +19,35 @@ func NewReader(conn net.Conn) *Reader {
 	}
 }
 
-func (r *Reader) ReadLine() []byte {
-	// TODO
+var ErrMalformedRequestLine = errors.New("malformed request line: missing CRLF")
 
-	return nil
+// ReadLine reads until delimiter '\n' from the reader and
+// checks for valid CRLF('\r\n').
+// It returns the line(string) without CRLF('\r\n').
+//
+// Note: It returns string because HTTP request line & headers
+// are ASCII text
+func (r *Reader) ReadLine() (string, error) {
+	line, err := r.reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	// Check for valid CRLF('\r\n')
+	if !strings.HasSuffix(line, "\r\n") {
+		return "", ErrMalformedRequestLine
+	}
+
+	return strings.TrimSuffix(line, "\r\n"), nil
 }
 
-func (r *Reader) ReadN(n int) []byte {
-	// TODO
+// ReadN reads n bytes from reader.
+// The bytes are taken from at most one Read on the underlying Reader,
+// hence bytes read may be less than n.
+func (r *Reader) ReadN(n int) (bytesRead int, buf []byte, err error) {
+	buf = make([]byte, n)
 
-	return nil
+	bytesRead, err = r.reader.Read(buf)
+
+	return
 }
