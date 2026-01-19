7784b2ffaa90a8eb3bf3b8f56e0d0731c9262ce0
suman <sumanmukherjee2001@gmail.com>
2026-01-19 23:54:23 +0530

    Add WriteHeader, Write, Header methods

--- http-server/response.go
+++ http-server/response.go
@@ -15,7 +15,30 @@ type response struct {
 
 	wroteHeader bool // Tells that a non 1xx header has been written
 
+	status int // status code passed to WriteHeader
+
 	w *bufio.Writer
 
 	contentLength int64
+}
+
+func (r *response) Header() Header {
+	return r.header
+}
+
+func (r *response) WriteHeader(code int) {
+	r.status = code
+}
+
+func (r *response) Write(data string) (int, error) {
+	// Write header if not written
+	if !r.wroteHeader {
+		r.WriteHeader(StatusOK)
+	}
+
+	if len(data) == 0 {
+		return 0, nil
+	}
+
+	return r.w.WriteString(data)
 }
