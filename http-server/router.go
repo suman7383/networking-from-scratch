25c6488074ff375a464f0406147fb0d605c8192c
suman <sumanmukherjee2001@gmail.com>
2026-01-21 00:21:23 +0530

    Add Get method

--- http-server/router.go
+++ http-server/router.go
@@ -5,3 +5,11 @@ type Router map[string]HandlerFunc
 func (r Router) AddRoute(pattern string, handler HandlerFunc) {
 	r[pattern] = handler
 }
+
+func (r Router) Get(pattern string) (handler HandlerFunc, ok bool) {
+	if h, ok := r[pattern]; !ok {
+		return nil, false
+	} else {
+		return h, true
+	}
+}
