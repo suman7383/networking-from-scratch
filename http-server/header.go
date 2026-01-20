d6631924c36a0786ad2ab6bfa5b94899d0c41304
suman <sumanmukherjee2001@gmail.com>
2026-01-20 23:35:45 +0530

    Updated to use CannonicalMIMEHeaderKey, added Set, Get

--- http-server/header.go
+++ http-server/header.go
@@ -1,23 +1,50 @@
 package main
 
-import "strings"
+import (
+	"net/textproto"
+	"strings"
+)
 
 // It represents the key-value pairs in an HTTP header
 type Header map[string][]string
 
-// Adds key, value pait to header
+// Adds key, value pair to header
 //
 // The key is case-insensitive
 func (h Header) Add(key, value string) {
 
-	// Convert key to lower case
-	kl := strings.ToLower(key)
+	// Convert key to canonicalMIMEHeaderKey
+	kl := textproto.CanonicalMIMEHeaderKey(key)
 	if _, ok := h[kl]; !ok {
 		h[kl] = make([]string, 0)
 	}
 
 	// Append the value at the given key
 	h[kl] = append(h[kl], value)
+}
+
+// Sets key to the new value
+func (h Header) Set(key, value string) {
+
+	kl := textproto.CanonicalMIMEHeaderKey(key)
+
+	// Delete any previous entry
+	delete(h, kl)
+
+	h[kl] = []string{value}
+}
+
+// Get returns the first value associated with the given key
+//
+// If no value for a key, it returns empty string
+func (h Header) Get(key string) string {
+	kl := textproto.CanonicalMIMEHeaderKey(key)
+
+	if _, ok := h[kl]; !ok {
+		return ""
+	}
+
+	return h[kl][0]
 }
 
 // Values returns all the values associated with the given key.
