1d3744097d42041d8cc35eddf9cdf9937ee56659
suman <sumanmukherjee2001@gmail.com>
2026-01-23 20:30:46 +0530

    Add reader that reads from conn

---
+++ websocket-server/internal/http/reader.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"strings"
+)
+
+// Reader is responsible for reading from the connection
+type Reader struct {
+	// reader is a buffered reader for the connection
+	reader *bufio.Reader
+}
+
+func NewReader(conn net.Conn) *Reader {
+	return &Reader{
+		reader: bufio.NewReader(conn),
+	}
+}
+
+var ErrExpectedTrailingCRLF = errors.New("expected trailing CRLF")
+
+// ReadLine reads until delimiter '\n' from the reader and
+// checks for valid CRLF('\r\n').
+// It returns the line(string) without CRLF('\r\n').
+//
+// Note: It returns string because HTTP request line & headers
+// are ASCII text
+func (r *Reader) ReadLine() (string, error) {
+	line, err := r.reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	// Check for valid CRLF('\r\n')
+	if !strings.HasSuffix(line, "\r\n") {
+		return "", ErrExpectedTrailingCRLF
+	}
+
+	return strings.TrimSuffix(line, "\r\n"), nil
+}
+
+// ReadN reads n bytes from reader.
+// The bytes are taken from at most one Read on the underlying Reader,
+// hence bytes read may be less than n.
+func (r *Reader) ReadN(n int) (bytesRead int, buf []byte, err error) {
+	buf = make([]byte, n)
+
+	bytesRead, err = r.reader.Read(buf)
+
+	return
+}
